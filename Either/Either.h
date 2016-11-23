dda857ee0988f7bf05780251f57d5460d2290a61
demons <837940593@qq.com>
2016-11-23 16:32:27 +0800

    .

---
+++ Either/Either.h
@@ -0,0 +1,99 @@
+#ifndef Either_H
+#define Either_H
+
+#include "../Show.h"
+#include "../Monad.h"
+#include "../TrivalValue.h"
+#include <functional>
+#include <type_traits>
+
+template <class LeftType, class RightType>
+class Either
+{
+    private:
+        const LeftType leftValue;
+        const RightType rightValue;
+        const bool isLeft;
+        
+    private:
+        /* 防止Type一致导致的函数相同类型 */
+        Either(const LeftType l, std::true_type);
+        Either(const RightType r, std::false_type);
+        
+    private:
+        template <class A, class B>
+        friend const Either<const A, const B> operator>>=
+        (
+            const Either<const A, const B> a,
+            const std::function<const Either<const A, const B>(const B)> func
+        );
+        
+        template <class A, class B>
+        friend const Either<const A, const B> Left(const A a);
+        
+        template <class A, class B>
+        friend const Either<const A, const B> Right(const B b);
+        
+        template <class A, class B>
+        friend const std::string show(const Either<const A, const B> a);
+};
+
+template <class LeftType, class RightType>
+Either<LeftType, RightType>::Either(const LeftType l, std::true_type):
+    leftValue(l),
+    rightValue(TrivalValue<const RightType>::value),
+    isLeft(true) {}
+
+template <class LeftType, class RightType>
+Either<LeftType, RightType>::Either(const RightType r, std::false_type):
+    leftValue(TrivalValue<const LeftType>::value),
+    rightValue(r),
+    isLeft(false) {}
+
+template <class A, class B>
+const Either<const A, const B> operator>>=
+(
+    const Either<const A, const B> a,
+    const std::function<const Either<const A, const B>(const B)> func
+)
+{
+    if (a.isLeft)
+    {
+        return a;
+    }
+    else
+    {
+        return func(a.rightValue);
+    }
+}
+
+template <class A, class B>
+const Either<const A, const B> Left(const A a)
+{
+    return Either<const A, const B>(a, std::true_type());
+}
+
+template <class A, class B>
+const Either<const A, const B> Right(const B b)
+{
+    return Either<const A, const B>(b, std::false_type());
+}
+
+template <class A, class B>
+const std::string show(const Either<const A, const B> a)
+{
+    typedef typename ImpShow<const A>::Has HasA;
+    typedef typename ImpShow<const B>::Has HasB;
+    static_assert(std::is_same<HasA, std::true_type>::value, "");
+    static_assert(std::is_same<HasB, std::true_type>::value, "");
+    if (a.isLeft)
+    {
+        return std::string("Left ") + show(a.leftValue);
+    }
+    else
+    {
+        return std::string("Right ") + show(a.rightValue);
+    }
+}
+
+#endif
---
+++ Either/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Either.h"
+
+int main()
+{
+    Either<const int, const char> a = Left<const int, const char>(2);
+    Either<const int, const int> b = Left<const int, const int>(3);
+    Either<const int, const int> c = Right<const int, const int>(3);
+    std::function<const Either<const int, const char>(const char)>
+    f = [](const char)
+    {
+        return Left<const int, const char>(2);
+    };
+    std::cout << show(a) << std::endl;
+    std::cout << show(b) << std::endl;
+    std::cout << show(c) << std::endl;
+    std::cout << show(a >>= f) << std::endl;
+}
