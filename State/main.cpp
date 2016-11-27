bf5518f0bd742eaf7e715da707d46caa72301a85
demons <837940593@qq.com>
2016-11-27 16:25:16 +0800

    引入多个Monad，inject会有冲突，用Monad::inject解决就好，>>=和>>不会有冲突

--- State/main.cpp
+++ State/main.cpp
@@ -1,10 +1,12 @@
 #include "State.h"
-#include "Maybe.h"
+#include "StateChar.h"
+#include "../Maybe/Maybe.h"
 #include <iostream>
 
 int main()
 {
     Monad<StateChar>::inject(2);
+    /* const State<const char, const int> x = inject(2); */
     const function<const typename apply_wrap1<StateChar, const int>::type(const int)>
     f = [](const int a)
     {
@@ -12,6 +14,7 @@ int main()
     };
     Monad<StateChar>::bind<const int, const int>(Monad<StateChar>().inject(2), f);
     std::cout << Monad<StateChar>::bind<const int, const int>(Monad<StateChar>().inject(2), f).evalState('c') << std::endl;
+    std::cout << (Monad<StateChar>::inject(2) >>= f).evalState('c') << std::endl;
     /* std::cout << (Monad<StateChar>().inject(2)).evalState('c') << std::endl; */
     const State<const char, const int> s = Monad<StateChar>::inject(2);
     std::cout << s.evalState('c') << std::endl;
@@ -23,10 +26,13 @@ int main()
         return Monad<MaybeWrapper>().inject(a + 1);
     };
     std::cout << Show<const Maybe<const int> >::show(Monad<MaybeWrapper>::bind<const int, const int>(Monad<MaybeWrapper>::inject(2), g)) << std::endl;
+    std::cout << show(Monad<MaybeWrapper>::inject(2) >>= g) << std::endl;
     const Maybe<const int> x = Monad<MaybeWrapper>::bind<const int, const int>(Monad<MaybeWrapper>::inject(2), g);
+    std::cout << show(Monad<MaybeWrapper>::inject(2) >>= g) << std::endl;
     const Maybe<const int> y = Monad<MaybeWrapper>::bind<const int, const int>(x, g);
     std::cout << Show<const Maybe<const int> >::show(y) << std::endl;
-    /* std::cout << Show<const Maybe<const int> >::show(Monad<MaybeWrapper>::bind<const int, const int>(Monad<MaybeWrapper>::bind<const int, const int>(Monad<MaybeWrapper>::inject(2), g)), g) << std::endl; */
+    std::cout << show(y) << std::endl;
+    std::cout << show((Monad<MaybeWrapper>::inject(2) >>= g) >>= g) << std::endl;
     const Maybe<const int> a = Just<const int>(2);
     const Maybe<const int> b = Nothing<const int>();
     const Maybe<const char> c = Just<const char>('t');
@@ -34,5 +40,7 @@ int main()
     const Maybe<const Maybe<const int> > e = Just<const Maybe<const int> >(a);
     const Maybe<const Maybe<const int> > h = Nothing<const Maybe<const int> >();
     std::cout << Show<const Maybe<const Maybe<const int> > >::show(e) << std::endl;
+    std::cout << show(e) << std::endl;
     std::cout << Show<const Maybe<const int> >::show(b) << std::endl;
+    std::cout << show(b) << std::endl;
 }
